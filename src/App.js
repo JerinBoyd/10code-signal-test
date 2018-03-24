438df468d5ae307ba3fbe0e91ffc938068b21ed1
JerinBoyd <maxers@mac.com>
2018-03-23 20:05:43 -0400

    added divs and lists to app.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
 class App extends Component {
+
+  
   render() {
+    
     return (
      <div>
-       Hello
+      <h1>Pick One!</h1>
+      <div className='border' > Test Questions
+      <ul>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+      </ul>
+       </div>
+      <button>Check Answer</button>
+      
      </div>
     );
   }

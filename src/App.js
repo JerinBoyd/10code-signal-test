6c6bd2262679fa16dd67cada80c56c42c5965f44
JerinBoyd <maxers@mac.com>
2018-03-28 18:30:21 -0400

    added answers to the questions

--- src/App.js
+++ src/App.js
@@ -18,10 +18,23 @@ class App extends Component {
             <input type="radio" name="q1" value='incorrect' /> Receiving Well <br/>
             <input type="radio" name="q1" value='correct' /> Receiving Poorly <br/>
             <input type="radio" name="q1" value='incorrect' /> Emergency Traffic <br/>
-            <input type="radio" name="q1" value='incoreect'/> Acknowledged <br/>
+            <input type="radio" name="q1" value='incorrect'/> Acknowledged <br/>
+            
           </ul>
         <li>10-2</li>
+        <ul>
+        <input type="radio" name="q1" value='incorrect'/> Stop Transmitting <br/>
+        <input type="radio" name="q1" value='incorrect'/> Emergency Traffic <br/>
+        <input type="radio" name="q1" value='incorrect'/> Disregard <br/>
+        <input type="radio" name="q1" value='correct'/> Receiving Well <br/>
+        </ul>
         <li>10-3</li>
+        <ul>
+        <input type="radio" name="q1" value='incorrect'/> Speaking Too Fast <br/>
+        <input type="radio" name="q1" value='incorrect'/> Receiving Poorly <br/>
+        <input type="radio" name="q1" value='incorrect'/> Stop Transmitting <br/>
+        <input type="radio" name="q1" value='incorrect'/> Emergency Traffic <br/>
+        </ul>
         </ol>
         <button type="submit">Submit</button>
         <button type="reset">Reset</button>

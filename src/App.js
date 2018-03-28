e35946349bfc786d0d1e626dc87763614dd67e8a
JerinBoyd <maxers@mac.com>
2018-03-28 19:44:10 -0400

    added 3 questions and submit button to each with a console.log to make sure button works

--- src/App.js
+++ src/App.js
@@ -1,52 +1,70 @@
-import React, { Component } from 'react';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
-import './App.css';
+import React, { Component } from "react";
+import Bootstrap from "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
-  
-  render() {
-    
-    return (
-     <div>
-    <body> 
-      <h1>10Code Quiz</h1>
-      <form>
-        <ol>
-        <li>10-1</li>
-          <ul>
-            <input type="radio" name="q1" value='incorrect' /> Receiving Well <br/>
-            <input type="radio" name="q1" value='correct' /> Receiving Poorly <br/>
-            <input type="radio" name="q1" value='incorrect' /> Emergency Traffic <br/>
-            <input type="radio" name="q1" value='incorrect'/> Acknowledged <br/>
-            
-          </ul>
-        <li>10-2</li>
-        <ul>
-        <input type="radio" name="q1" value='incorrect'/> Stop Transmitting <br/>
-        <input type="radio" name="q1" value='incorrect'/> Emergency Traffic <br/>
-        <input type="radio" name="q1" value='incorrect'/> Disregard <br/>
-        <input type="radio" name="q1" value='correct'/> Receiving Well <br/>
-        </ul>
-        <li>10-3</li>
-        <ul>
-        <input type="radio" name="q1" value='incorrect'/> Speaking Too Fast <br/>
-        <input type="radio" name="q1" value='incorrect'/> Receiving Poorly <br/>
-        <input type="radio" name="q1" value='incorrect'/> Stop Transmitting <br/>
-        <input type="radio" name="q1" value='incorrect'/> Emergency Traffic <br/>
-        </ul>
-        </ol>
-        <button type="submit">Submit</button>
-        <button type="reset">Reset</button>
-      </form>
-    </body>
+    this.state = {
+      questions: [
+        
+        {
+          number: "10-1",
+          answers: [
+            'Receiving Poorly',
+            'Receiving Well',
+            'Emergency Traffic'
+          ]
+        },
+        {
+          number: "10-2",
+          answers: [
+            'Acknowledged',
+            'Receiving Poorly',
+            'Receiving Well'
+          ]
+        },
+        {
+          number: "10-3",
+          answers: [
+            'Disregard',
+            'Speaking Too Fast',
+            'Stop Transmitting'
+          ]
+        },
       
-     </div>
+      ]
+    };
+  }
+
+ 
+check = () => {
+  console.log('checked')
+}
+  render() {
+    return (
+      <div>
+        <body>
+          <h1>10Code Quiz</h1>
+
+          {this.state.questions.map(q => (
+            <div>
+              {" "}
+              <h1>{q.number}</h1>
+              <ul >
+                {q.answers.map(answer => <li className="dots" >
+                 <input type="radio" name="" id=""/> {answer}
+                </li  > )}
+              </ul>
+               <button onClick={this.check} >Submit</button>{" "}
+              <button type="reset">Reset</button>{" "}
+            </div>
+          ))}
+        </body>
+      </div>
     );
   }
 }
-
-
-
 
 export default App;
